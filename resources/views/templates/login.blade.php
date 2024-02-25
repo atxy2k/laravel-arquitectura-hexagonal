c5773b543e1be7449384d64dde7aaf8f28a80559
Esteban Sahid <esteban_ct28@hotmail.com>
2024-02-24 22:14:43 -0500

    prueba pr

--- resources/views/templates/login.blade.php
+++ resources/views/templates/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
     <!-- Custom fonts for this template-->
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
